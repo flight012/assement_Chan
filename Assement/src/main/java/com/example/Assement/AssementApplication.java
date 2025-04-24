352270fea65a109cc5351b0f8559750b56cae366
scchan <szechee.chan@silverlakeaxis.com>
2025-04-24 14:52:29 +0800

    commit

---
+++ Assement/src/main/java/com/example/Assement/AssementApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssementApplication.class, args);
+	}
+
+}
